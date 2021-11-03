3c000bfda8d4429842e21ac2b6bcc4f56d176334
yejun <oagree0123@naver.com>
2021-11-03 18:09:28 +0900

    ✨ Implement user-gallery-thumbnail and its controls

--- js/carousels.js
+++ js/carousels.js
@@ -16,6 +16,8 @@ const userGallery = tns({
   gutter: 4,
   edgePadding: 16,
   controls: false,
+  controlsContainer: '.user-gallery-controls',
+  navContainer: '.user-gallery .thumbnail-list',
   loop: false,
   arrowKeys: true,
   mouseDrag: true,
